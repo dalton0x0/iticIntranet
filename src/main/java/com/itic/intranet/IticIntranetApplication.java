815b8945130daa826f7a744f5bfbccc2178d7e36
Chéridanh <divintsiela@gmail.com>
2025-05-11 20:55:11 +0200

    add dotenv config and configure .env and application.properties file

--- src/main/java/com/itic/intranet/IticIntranetApplication.java
+++ src/main/java/com/itic/intranet/IticIntranetApplication.java
@@ -1,19 +1,21 @@
 package com.itic.intranet;
 
-import org.springframework.boot.CommandLineRunner;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IticIntranetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IticIntranetApplication.class, args);
-    }
 
-    @Bean
-    public CommandLineRunner init() {
-        return args -> {};
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("MYSQL_CONNECTION", dotenv.get("MYSQL_CONNECTION"));
+        System.setProperty("MYSQL_USERNAME", dotenv.get("MYSQL_USERNAME"));
+        System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+        System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+        System.setProperty("MONGODB_DATABASE", dotenv.get("MONGODB_DATABASE"));
+
+        SpringApplication.run(IticIntranetApplication.class, args);
     }
 }
