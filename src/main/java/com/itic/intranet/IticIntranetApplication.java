e50188e9a13b7bb53aa8c9bed8bd38e7f47dd26f
Chéridanh <divintsiela@gmail.com>
2025-06-06 16:55:46 +0200

    Updating and separate environment variables for docker and local host

--- src/main/java/com/itic/intranet/IticIntranetApplication.java
+++ src/main/java/com/itic/intranet/IticIntranetApplication.java
@@ -10,9 +10,14 @@ public class IticIntranetApplication {
     public static void main(String[] args) {
 
         Dotenv dotenv = Dotenv.load();
+
         System.setProperty("SPRING_APPLICATION_NAME", dotenv.get("SPRING_APPLICATION_NAME"));
-        System.setProperty("SPRING_DATASOURCE_URL_LOCAL", dotenv.get("SPRING_DATASOURCE_URL_LOCAL"));
+        System.setProperty("SPRING_DATASOURCE_DRIVER_CLASSNAME", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASSNAME"));
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
         System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("SPRING_JPA_DATABASE_PLATFORM", dotenv.get("SPRING_JPA_DATABASE_PLATFORM"));
+        System.setProperty("SPRING_JPA_HIBERNATE_DDL_AUTO", dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
         System.setProperty("SPRING_DATA_MONGODB_URI", dotenv.get("SPRING_DATA_MONGODB_URI"));
         System.setProperty("SPRING_DATA_MONGODB_DATABASE", dotenv.get("SPRING_DATA_MONGODB_DATABASE"));
         System.setProperty("JWT_TOKEN", dotenv.get("JWT_TOKEN"));
