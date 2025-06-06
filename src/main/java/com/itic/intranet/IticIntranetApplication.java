ec106f38016117af75bef60581239a2a10479f47
Chéridanh <divintsiela@gmail.com>
2025-06-06 15:40:17 +0200

    Updating environment variables

--- src/main/java/com/itic/intranet/IticIntranetApplication.java
+++ src/main/java/com/itic/intranet/IticIntranetApplication.java
@@ -10,10 +10,11 @@ public class IticIntranetApplication {
     public static void main(String[] args) {
 
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("MYSQL_CONNECTION_LOCALE", dotenv.get("MYSQL_CONNECTION_LOCALE"));
-        System.setProperty("MYSQL_USERNAME", dotenv.get("MYSQL_USERNAME"));
-        System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
-        System.setProperty("MONGODB_DATABASE", dotenv.get("MONGODB_DATABASE"));
+        System.setProperty("SPRING_APPLICATION_NAME", dotenv.get("SPRING_APPLICATION_NAME"));
+        System.setProperty("SPRING_DATASOURCE_URL_LOCAL", dotenv.get("SPRING_DATASOURCE_URL_LOCAL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATA_MONGODB_URI", dotenv.get("SPRING_DATA_MONGODB_URI"));
+        System.setProperty("SPRING_DATA_MONGODB_DATABASE", dotenv.get("SPRING_DATA_MONGODB_DATABASE"));
         System.setProperty("JWT_TOKEN", dotenv.get("JWT_TOKEN"));
 
         SpringApplication.run(IticIntranetApplication.class, args);
