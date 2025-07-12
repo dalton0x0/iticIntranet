fc3a7f567f0868b6cfece59fee78e91787520453
Chéridanh <divintsiela@gmail.com>
2025-07-12 19:25:26 +0200

    update cors url wthite list

--- src/main/java/com/itic/intranet/configs/SecurityConfig.java
+++ src/main/java/com/itic/intranet/configs/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:63343", "http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:63344", "http://localhost:5173"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
